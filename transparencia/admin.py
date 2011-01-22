9db5a1ad36e6f169d7444d593b1a6f6e5a8296d8
Julian Pulgarin <jp@julianpulgarin.com>
2011-01-22 04:51:30 -0500

    Perfeccionando admin, arreglando models (upload path + default values

--- transparencia/admin.py
+++ transparencia/admin.py
@@ -3,15 +3,26 @@ from django.contrib import admin
 
 from transparencia.models import Promesa, Personaje, Cargo, PromesaCargo, Etiqueta, PromesaEtiqueta
 
+class PromesaCargoInline(admin.StackedInline):
+    model = PromesaCargo
+    extra = 1
+
+class PromesaEtiquetaInline(admin.StackedInline):
+    model = PromesaEtiqueta
+    extra = 1
+
 class PromesaAdmin(admin.ModelAdmin):
+    inlines = [PromesaCargoInline, PromesaEtiquetaInline]
     prepopulated_fields = {"slug": ("titulo",)}
 
+class CargoInline(admin.StackedInline):
+    model = Cargo
+    extra = 1
+
 class PersonajeAdmin(admin.ModelAdmin):
+    inlines = [CargoInline,]  
     prepopulated_fields = {"slug": ("nombre",)}
 
 admin.site.register(Promesa, PromesaAdmin)
 admin.site.register(Personaje, PersonajeAdmin)
-admin.site.register(Cargo)
-admin.site.register(PromesaCargo)
 admin.site.register(Etiqueta)
-admin.site.register(PromesaEtiqueta)
--- transparencia/models.py
+++ transparencia/models.py
@@ -19,20 +19,21 @@ class Promesa(models.Model):
     slug = models.SlugField()
     estatus = models.IntegerField(choices=ESTATUS_OPCIONES)
     descripcion = models.TextField()
-    arriba = models.IntegerField() #default=0?
-    abajo = models.IntegerField()
-    compartido = models.IntegerField()
+    arriba = models.IntegerField(default=0)
+    abajo = models.IntegerField(default=0)
+    compartido = models.IntegerField(default=0)
     
     def __unicode__(self):
         return self.titulo
-
 
 class Personaje(models.Model):
     nombre = models.CharField(max_length=64)
     slug = models.SlugField()
     descripcion = models.TextField()
-    image = models.ImageField(upload_to='media/img/personajes') #test
+    image = models.ImageField(upload_to='img/personajes')
 
+    def __unicode__(self):
+        return self.nombre
 
 class Cargo(models.Model):
     personaje = models.ForeignKey(Personaje)
@@ -40,19 +41,22 @@ class Cargo(models.Model):
     inicio = models.DateField()
     terminacion = models.DateField(blank=True, null=True)
 
+    def __unicode__(self):
+        return "%s, %s, %s - %s" % (self.personaje.nombre, self.titulo, self.inicio.year, self.terminacion.year)
+
     class Meta:
         unique_together = ('personaje', 'titulo', 'inicio')
-
 
 class PromesaCargo(models.Model):
     promesa = models.ForeignKey(Promesa)
     cargo = models.ForeignKey(Cargo)
 
-
 class Etiqueta(models.Model):
     texto = models.CharField(max_length=32, unique=True)
     descripcion = models.TextField()
 
+    def __unicode__(self):
+        return self.texto
 
 class PromesaEtiqueta(models.Model):
     promesa = models.ForeignKey(Promesa)
