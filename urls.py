625be84370ffaa868c3c11fd963a3ef6e92565ed
Julian Pulgarin <jp@julianpulgarin.com>
2011-01-22 18:33:54 -0500

    Base

--- urls.py
+++ urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^etiqueta/(?P<slug>[-\w]+)/$', etiqueta, name='etiqueta'),
     url(r'^promesa/(?P<slug>[-\w]+)/$', promesa, name='promesa'),
     url(r'^personaje/(?P<slug>[-\w]+)/$', personaje, name='personaje'),
+    (r'^api/', include('api.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     
