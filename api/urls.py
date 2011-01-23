963a3a4749cfae19f02c22979ce0dc3823170714
Julian Pulgarin <jp@julianpulgarin.com>
2011-01-23 18:39:52 -0500

    APi changes

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('api.views',
     # GET API
     url(r'^buscar_tags.json$', 'buscar_tags'),
-    url(r'^buscar_promesas.json$', 'buscar_promesas'),
+    url(r'^buscar_promesas/$', 'buscar_promesas'),
 )
